c7e87b6e6d18465ce04c8c5a6a811d3681ddaafe
Naomi Kapele <naomikapele@booknowzambia.com>
2024-06-28 14:20:15 +0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// import ``
+// import `http` core module
+const http = require('http');
+
+// create server
+const server = http.createServer((req,res) => {
+
+});
+
+
+// listen to incoming requests on port 3000
+server.listen(3000);
